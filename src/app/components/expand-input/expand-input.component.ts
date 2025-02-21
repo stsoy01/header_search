441d4045d32096e7b3f8bbc08f0605122e8c7a0a
stsoy01 <tsoy@perco.ru>
2025-02-21 18:35:11 +0300

    upd rewrite extand opening logic

--- src/app/components/expand-input/expand-input.component.ts
+++ src/app/components/expand-input/expand-input.component.ts
@@ -1,43 +1,66 @@
 import {
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  HostListener,
-  Output,
-  QueryList,
-  Renderer2,
-  ViewChildren,
-  EventEmitter,
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    HostListener,
+    Output,
+    QueryList,
+    Renderer2,
+    ViewChildren,
+    EventEmitter,
+    ViewChild,
+    Input,
+    OnDestroy,
 } from '@angular/core';
+import {ExpandInputService} from "./expand-input.service";
 
 @Component({
-  selector: 'app-expand-input',
-  templateUrl: './expand-input.component.html',
-  styleUrls: ['./expand-input.component.scss'],
-  standalone: true,
-  imports: [
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-expand-input',
+    templateUrl: './expand-input.component.html',
+    styleUrls: ['./expand-input.component.scss'],
+    standalone: true,
+    imports: [],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class ExpandInputComponent {
+export class ExpandInputComponent implements OnDestroy {
 
-  @Output() public isNgContentDisplayed = new EventEmitter<boolean>();
+    @Input() public asd = false;
+    @Output() public isNgContentDisplayed = new EventEmitter<boolean>();
 
-  constructor(
-    private el: ElementRef,
-    private renderer: Renderer2) {
-  }
+    private isInside = false;
+    private clearId!: unknown;
 
-  @ViewChildren('div') private expandContainer!: QueryList<ElementRef>;
+    constructor(
+        private el: ElementRef,
+        private renderer: Renderer2,
+        private expandService: ExpandInputService) {
+    }
 
-  @HostListener('click', ['$event']) private onElementClick(): void {
-    const [, expandElement] = this.expandContainer.toArray();
-    const isInside = !!this.el.nativeElement.contains(event?.target);
+    @ViewChild('addBtn') private addBtn!: ElementRef<HTMLButtonElement>
 
-    console.log();
-    
-    this.isNgContentDisplayed.emit(isInside)
-    this.renderer.setStyle(expandElement.nativeElement, 'width', isInside ? '500px' : '0px')
-  }
+    @ViewChildren('div') private expandContainer!: QueryList<ElementRef>;
+
+    @HostListener('document:click', ['$event.target']) private onElementClick(): void {
+        const [, expandElement] = this.expandContainer.toArray();
+        this.isInside = !!this.el.nativeElement.contains(event?.target);
+
+        if ((event?.target as HTMLElement).id === 'addBtn') return;
+
+        if (this.isInside) {
+            this.isNgContentDisplayed.emit(this.isInside)
+            this.renderer.setStyle(expandElement.nativeElement, 'width', '704px')
+            this.renderer.setStyle(expandElement.nativeElement, 'height', this.expandService.asd ? '250px' : '48px')
+        } else {
+            this.expandService.asd = false;
+            this.renderer.setStyle(expandElement.nativeElement, 'height', this.expandService.asd ? '250px' : '48px')
+            this.clearId = setTimeout(() => {
+                this.renderer.setStyle(expandElement.nativeElement, 'width', '0')
+            }, 175)
+        }
+    }
+
+    public ngOnDestroy(): void {
+        clearTimeout(this.clearId as number)
+    }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,45 +1,76 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AsyncPipe, NgClass, NgIf, NgStyle, NgTemplateOutlet } from '@angular/common';
-import { ExpandInputComponent } from '../expand-input/expand-input.component';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import { Observable} from 'rxjs';
+import {AsyncPipe, NgClass, NgIf, NgStyle, NgTemplateOutlet} from '@angular/common';
+import {ExpandInputComponent} from '../expand-input/expand-input.component';
+import {ExpandInputService} from "../expand-input/expand-input.service";
 
-interface headerNavigationItemInterface {
-  linkName: string;
-  iconUrl?: string
+interface HeaderNavigationItemInterface {
+    linkName: string;
+    iconUrl?: string
+}
+
+interface UserDataInterface {
+    id: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    notification_count: number;
+    is_block: boolean;
+    avatar_url: string;
 }
 
 @Component({
-  selector: 'app-header',
-  imports: [
-    RouterLink,
-    AsyncPipe,
-    NgTemplateOutlet,
-    NgClass,
-    NgStyle,
-    ExpandInputComponent,
-    NgIf,
-  ],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
-  standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-header',
+    imports: [
+        RouterLink,
+        AsyncPipe,
+        NgTemplateOutlet,
+        NgClass,
+        NgStyle,
+        ExpandInputComponent,
+        NgIf,
+    ],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss',
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent implements OnInit {
 
-  public headerNavigationList$: Observable<headerNavigationItemInterface[]> = new Observable((subscriber) => {
-    subscriber.next(
-      [
-        { linkName: 'Ссылки', iconUrl: '/assets/img/link.png' },
-        { linkName: 'Контакты', iconUrl: '/assets/img/contacts.png' },
-        { linkName: 'Теги', iconUrl: '/assets/img/tag.png' },
-        { linkName: 'Избранное', iconUrl: '/assets/img/favorites.png' },
-        { linkName: 'Посещения', iconUrl: '/assets/img/history.png' },
-      ])
-  })
+    public headerNavigationList$: Observable<HeaderNavigationItemInterface[]> = new Observable((subscriber) => {
+        subscriber.next(
+            [
+                {linkName: 'Ссылки', iconUrl: '/assets/img/link.png'},
+                {linkName: 'Контакты', iconUrl: '/assets/img/contacts.png'},
+                {linkName: 'Теги', iconUrl: '/assets/img/tag.png'},
+                {linkName: 'Избранное', iconUrl: '/assets/img/favorites.png'},
+                {linkName: 'Посещения', iconUrl: '/assets/img/history.png'},
+            ])
+    })
 
-  public isContentShowed: boolean = false;
+    public userData$: Observable<UserDataInterface> = new Observable((subscriber) => {
+        subscriber.next({
+            id: 1,
+            avatar_url: "/assets/img/avatar.png",
+            email: "j_ren@gmail.com",
+            first_name: "Jeremy",
+            is_block: false,
+            last_name: "Renner",
+            notification_count: 11
+        })
+    })
 
-  ngOnInit(): void {
-  }
+    constructor(public expandService: ExpandInputService) {
+    }
+
+    public isContentShowed: boolean = false;
+
+    ngOnInit(): void {
+    }
+
+    public changeService(): void {
+        this.expandService.asd = true;
+    }
+
 }
